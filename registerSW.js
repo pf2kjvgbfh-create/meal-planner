5dc1e903110154153a33b34c575029ff028ac016
pf2kjvgbfh-create <pf2kjvgbfh@privaterelay.appleid.com>
2026-05-05 12:42:07 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/meal-planner/sw.js', { scope: '/meal-planner/' })})}
